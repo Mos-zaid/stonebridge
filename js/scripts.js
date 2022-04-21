7f718270a22656681baf5a1fd8fadcc6ead3d3a0
Zaid <freeze4allems@gmail.com>
2022-04-21 17:46:02 +0100

    Working on Carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 
+// OWL Carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        dots: true,
+        nav:true,
+        // margin:10,
+        autoplay:true,
+        autoplayTimeout:2500,
+        smartSpeed:1500,
+        autoplayHoverPause:false,
+    });
+});
 
-// Carousel
