65df9a63e175a2e327c11e439fc522acf1dba49e
Zaid <freeze4allems@gmail.com>
2022-04-23 13:30:15 +0100

    Finished Working Carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,76 @@
 
 // OWL Carousel
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         items:1,
+//         loop:true,
+//         dots: true,
+//         nav:true,
+//         // margin:10,
+//         autoplay:true,
+//         autoplayTimeout:3500,
+//         smartSpeed:1500,
+//         autoplayHoverPause:false,
+//     });
+
+// });
+
+
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        loop:true,
-        dots: true,
-        nav:true,
-        // margin:10,
+    $('.owl-one').owlCarousel({
         autoplay:true,
         autoplayTimeout:3500,
         smartSpeed:1500,
-        autoplayHoverPause:false,
+        loop:true,
+        // margin:10,
+        dots: false,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
     });
 
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('.owl-three').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
 });
 
 
